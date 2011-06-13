17d5e1dba2ca015479937c466a652cb51d4fc658
Vivek Narayanan <mail@vivekn.co.cc>
2011-06-13 21:10:22 +0530

    Added 'friends' feature

--- resn.py
+++ resn.py
@@ -16,7 +16,7 @@ def create_user(id_attr = None, **kwargs):
     """
     Creates a new user with attributes specified as keyword arguments
 
-    'id_attr' is an optional argument to set the attribute that will uniquely identify a user.
+    'id_attr' is the name of the attribute that will uniquely identify the user.
     """
     get_redis().incr("users.counter")
     ctr = get_redis().get("users.counter")
@@ -24,11 +24,10 @@ def create_user(id_attr = None, **kwargs):
     for key in kwargs:
         user[key] = kwargs[key]
    
-    if id_attr:
-        if id_attr not in kwargs:
-            raise KeyError, "You did not specify any key '%s'." % id_attr
-        else:
-            get_redis().set("users.id.%s" % kwargs[id_attr], ctr)
+    if id_attr not in kwargs:
+        raise KeyError, "You did not specify any key '%s' in the call to this function." % id_attr
+    else:
+        get_redis().set("users.id.%s" % kwargs[id_attr], ctr)
 
 def get_user_by_id(user_id):
     """Returns an editable dictionary like wrapper of a user by numeric id"""
@@ -36,11 +35,19 @@ def get_user_by_id(user_id):
 
 def get_user(user):
     """
-    Requires 'id_attr' to be set when create_user was called.
     Returns an editable dictionary like wrapper based on the attribute set in 'id_attr'
     """
     return get_hash('users.id.' + user)
 
+def create_connection_by_ids(user1, user2):
+    """Creates a 'friendship' between two users. Uses internal numeric ids"""
+    u1 = get_set('users.%s.friends' % user1)
+    u2 = get_set('users.%s.friends' % user2)
+    u1.add(user2)
+    u2.add(user1)
 
-
-
+def create_connection(user1, user2):
+    """Creates a 'friendship' between two users, parameters are the values of the 'id_attr' set in 'create_user'"""
+    u1 = get_redis().get('users.id.' + user1)
+    u2 = get_redis().get('users.id.' + user2)
+    create_connection_by_ids(u1, u2)
