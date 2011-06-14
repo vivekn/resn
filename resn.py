855bff3f194c61a485db17caa0138bf14438210d
Vivek Narayanan <mail@vivekn.co.cc>
2011-06-14 14:03:04 +0530

    Added updates and user feed

--- resn.py
+++ resn.py
@@ -12,7 +12,7 @@ License: BSD
 
 from redis_wrap import get_redis, get_hash, get_set
 
-def create_user(id_attr = None, **kwargs):
+def create_user(id_attr, **kwargs):
     """
     Creates a new user with attributes specified as keyword arguments
 
@@ -28,6 +28,7 @@ def create_user(id_attr = None, **kwargs):
         raise KeyError, "You did not specify any key '%s' in the call to this function." % id_attr
     else:
         get_redis().set("users.id.%s" % kwargs[id_attr], ctr)
+        user['id'] = kwargs[id_attr]
 
 def get_user_by_id(user_id):
     """Returns an editable dictionary like wrapper of a user by numeric id"""
@@ -51,3 +52,43 @@ def create_connection(user1, user2):
     u1 = get_redis().get('users.id.' + user1)
     u2 = get_redis().get('users.id.' + user2)
     create_connection_by_ids(u1, u2)
+    
+def new_update(user, **update):
+    """Creates a new update object and pushes the update to the feeds of the user's friends and followers.
+    The parameter 'user' is the numeric id of the user"""
+
+    get_redis().incr("updates.counter")
+    ctr = get_redis().get("updates.counter")
+    upd = get_hash("updates." + ctr)
+    upd['user'] = user
+    for key in update:
+        upd = update[key]
+
+    friends = get_set("users.%s.friends" % user)
+    for friend in friends:
+        get_redis().lpush("users.%s.feed" % friend, ctr)
+        get_redis().ltrim("users.%s.feed" % friend, 0, 1000) #TODO: Change hardcoded limit 1000 to a setting.
+
+    get_redis().lpush("users.%s.updates" % user, ctr) #This is useful for maintaining a list of updates by a particular user
+    
+def delete_update(update_id):
+    """Deletes a particular update"""
+    get_redis().delete("updates." + update_id)
+
+def get_updates_from_list(feed):
+    """ Performs existence checks on updates """
+    newfeed = []
+    for update in feed:
+        if len(update):
+            newfeed.append(update)
+    return newfeed
+
+def get_feed(user, limit = 1000):
+    """Returns the feed containing updates by friends"""
+    return get_updates_from_list(get_redis.lrange("users.%s.feed" % user, 0, limit))
+
+def get_user_updates(user, limit = 1000):
+    """Returns a list of updates by the user. Useful for generating a user profile."""
+    return get_updates_from_list(get_redis.lrange("users.%s.updates" % user, 0, limit))
+
+        
